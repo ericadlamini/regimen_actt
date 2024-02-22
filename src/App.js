c6fd6a191d6043a75efdcbe575a9f80955697c75
ericadlamini <erica.thamie@gmail.com>
2024-02-22 22:44:08 +0200

    Added initial commit, imported axios, jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city.."
+        onChange={updateCity}
+        className="search-form-input"
+      />
+      <button type="Submit" className="search-form-button">
+        Search
+      </button>
+    </form>
   );
-}
 
-export default App;
+  if (loaded) {
+    return (
+      <div className="App">
+        {form}
+        <ul>
+          <li className="temperature">{Math.round(weather.temperature)}°C</li>
+          <li className="city">in {city}</li>
+          <li>Conditions: {weather.description}</li>
+          <li>
+            Humidity: <span className="details">{weather.humidity}%</span>
+          </li>
+          <li>
+            Wind: <span className="details">{weather.wind}km/h</span>
+          </li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+        <div className="footer">
+          This app was Coded by{" "}
+          <a href="https://github.com/ericadlamini" target="blank">
+            Erica Dlamini
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/ericadlamini/myweather-app"
+            target="_blank"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://mypowerfulweather.netlify.app/" target="_blank">
+            Netlify
+          </a>
+        </div>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
